a35c4ab0a6effee9a01ca40cf90b1dd409792ed8
butterrzz <38452206+butterrzz@users.noreply.github.com>
2018-06-14 12:37:26 -0700

    hw update making game better

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,85 @@
-var randomWordArr = ["javascript","bacon","beer","programming","gaming"]
-var randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
 
-var s;
-var count = 0;
-var answerArray = [];
+//creating global variables//
+var wordList = ["webdev","javascript","programming","life","reading","glory","fantastic"]
+var chosenWord = "";
+var letterInChosenWord = [];
+var numBlanks= 0;
+var blanksAndSuccesses = [];
+var wrongGuesses = [];
 
-function startUp()
-{
-        for (var i = 0; i < randomWord.length; i++)
-        {
-            answerArray[i] = "_";
-        }
-        s = answerArray.join(" ");
-        document.getElementById("answer").innerHTML = s;
-}
+var numGuesses = 15;
+var winCounter = 0;
+var lossCounter = 1;
 
-function Letter()
-{
-    var letter = document.getElementById("letter").value;
+function startGame(){
+    wrongGuesses = [];
+    numGuesses = 15;
+    blanksAndSuccesses = [];
+    //creatin our random word selector//
+    chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+    lettersInChosenWord = chosenWord.split("");
+    numBlanks = lettersInChosenWord.length;
 
-    if (letter.length > 0)
-    {   
-     for (var i = 0; i < randomWord.length; i++)
-     {
-         if (randomWord[i] === letter)
-     {
-        answerArray[i] = letter;
-     }
+    for(var i = 0; i < numBlanks; i++){
+        blanksAndSuccesses.push("_");
     }
-    count++;
-    document.getElementById("counter").innerHTML = "no of clicks: " + count;
-    document.getElementById("answer").innerHTML = answerArray.join(" ");
+    document.getElementById("word-blank").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("guesses-left").innerHTML = numGuesses;
+
 }
-if(count>12)
-{
-    document.getElementById("stat").innerHTML = "come on you should have gotten it by now!";
+//creating our letter checking function//
+function checkLetters(letter){
+
+    var letterInWord = false;
+
+    for(var i = 0; i < numBlanks; i++){
+        if(chosenWord[i] === letter){
+            letterInWord = true;
+
+        }
+    }
+
+    if(letterInWord){
+        for(i = 0; i < numBlanks; i++){
+            if(chosenWord[i] === letter){
+            blanksAndSuccesses[i] = letter;
+
+        }
+
+        }
+    }else{
+        numGuesses --;
+        wrongGuesses.push(letter)
+    }
 }
+//once we completed a round this functon will pretty much reset our game and keep score//
+function roundComplete(){
+    document.getElementById('word-blank').innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById('guesses-left').innerHTML = numGuesses;
+    document.getElementById('wrong-guesses').innerHTML = wrongGuesses.join(" ");    
+
+    if(lettersInChosenWord.join(" ") === blanksAndSuccesses.join(" ")){
+        winCounter++;
+        alert("You win!!");
+        document.getElementById('win-counter').innerHTML = winCounter;
+        startGame();
+    }else if(numGuesses === 0){
+        document.getElementById('loss-counter').innerHTML  = lossCounter ++;
+        document.getElementById('wrong-guesses').innerHTML = "";
+        alert("you don't have any more guesses");        
+        startGame();
+    }
+
+
+
+
+}
+startGame();
+document.onkeyup = function(event){
+    /* its going to take in the letter that we type in
+       its going to pass it through the CheckLetter function */
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed)
+    roundComplete();
+
 }
